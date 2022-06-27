4aedd701f46d83145cae63d511ba936886bfb39d
lizhihua <275091674@qq.com>
2022-06-27 17:24:59 +0800

    feat: update

--- src/controller/modules/ldapauth.js
+++ src/controller/modules/ldapauth.js
@@ -1,92 +1,51 @@
 import ldap from 'ldapjs';
 import Q from 'q';
 
+import Sequelize from 'sequelize';
 import config from '../../config';
-import { simpleParse } from '../../helpers/utils';
+import logger from '../../logger';
+
+const {
+    model
+} = config;
+
+const sequelize = new Sequelize('users', model.username, model.password, {
+    port: model.port,
+    host: model.host,
+    define: {
+        charset: 'utf8',
+        timestamps: false
+    },
+    logging: __DEVELOPMENT__ ?
+        logger.info.bind(logger) : false
+});
+
+sequelize.authenticate().then(() => logger.info('Connection has been established successfully.')).catch(err => logger.error('Unable to connect to the database:', err));
 
 function* ldapQuery(username, password) {
-  const deferred = Q.defer();
-
-  const { login } = config;
-
-  const client = ldap.createClient({
-    url: login.server,
-  });
-
-  client.once('error', (err) => {
-    if (err) {
-      deferred.reject(err);
-    }
-  });
-
-  const ldapSearch = (err, search) => {
-    const users = [];
-
-    if (err) {
-      deferred.reject(err);
-    }
-
-    search.on('searchEntry', (entry) => {
-      if (entry) {
-        users.push(entry.dn);
-      }
-    });
-
-    search.on('error', (e) => {
-      if (e) {
+    const deferred = Q.defer();
+    sequelize.query(`SELECT * FROM users WHERE user='${username}' and pwd=md5('${password}')`, {
+        type: sequelize.QueryTypes.SELECT
+    }).then((results) => {
+        deferred.resolve(!!results && !!results.length);
+    }).catch((err) => {
         deferred.resolve(false);
-      }
     });
-
-    search.on('searchReference', (referral) => {
-      if (referral) {
-        deferred.reject(referral);
-      }
-    });
-
-    search.on('end', () => {
-      if (users.length > 0) {
-        client.bind(users[0], password, (e) => {
-          if (e) {
-            deferred.resolve(false);
-          } else {
-            deferred.resolve(true);
-          }
-
-          client.unbind();
-        });
-      } else {
-        deferred.resolve(false);
-
-        client.unbind();
-      }
-    });
-  };
-
-  client.bind([login.bindDn, login.baseDn].join(','), login.bindPassword, (err) => {
-    if (err) {
-      deferred.reject(err);
-    }
-
-    const searchDn = simpleParse([login.searchDn, login.baseDn].join(','), { username });
-
-    const opts = {
-      filter: '(objectclass=*)',
-      scope: 'sub',
-    };
-
-    client.search(searchDn, opts, ldapSearch);
-  });
-
-  return deferred.promise;
+    return deferred.promise;
 }
 
+
 export function* getLdapAuth(next) {
-  const { username, password } = this.param;
+    const {
+        username,
+        password
+    } = this.param;
 
-  const isValid = yield ldapQuery(username, password);
+    const isValid = yield ldapQuery(username, password);
 
-  this.state.respond = { isValid };
+    this.state.respond = {
+        isValid
+    };
 
-  yield next;
+    yield next;
 }
